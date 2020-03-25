4a76fd915e20826d965cd39a53f49ff2ca5c9a4a
Paymanshus <paymanshus@gmail.com>
2020-03-25 23:41:09 +0530

    Add files via upload

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,7 +1,9 @@
-$('.js-modal-show').click(function() {
-    $('.js-modal-shopify').toggleClass('is-shown--off-flow').toggleClass('is-hidden--off-flow');
-});
+$(document).ready(function() {
+    $('.js-modal-show-fin').click(function() {
+        $('.js-modal-financial').toggleClass('is-shown--off-flow').toggleClass('is-hidden--off-flow');
+    });
 
-$('.js-modal-hide').click(function() {
-    $('.js-modal-shopify').toggleClass('is-shown--off-flow').toggleClass('is-hidden--off-flow');
+    $('.js-modal-hide-fin').click(function() {
+        $('.js-modal-financial').toggleClass('is-shown--off-flow').toggleClass('is-hidden--off-flow');
+    });
 });
